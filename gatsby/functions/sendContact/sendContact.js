8f634dd87962b7e0e779ca89e5816f6049333404
Luis Castro <luish.castroc@gmail.com>
2021-02-10 03:34:53 -0500

    added sendcontact function

---
+++ gatsby/functions/sendContact/sendContact.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+exports.handler = async (event, context) => {
+  const info = await transporter.sendMail({
+    from: 'Contact Marijo Fit Life <marijo.fit.life@gmail.com>',
+    to: 'marijo.fit.life@gmail.com',
+    subject: 'Contact Form',
+    html: `<p>Testing Contact</p>`,
+  });
+
+  console.log(info);
+
+  return {
+    statusCode: 200,
+    body: info,
+  };
+};
